488fb0d15e346bdc1ba0b1fafc7c4b112bb10e1b
Tamara <tamara.alonso@ikasle.egibide.org>
2019-01-14 22:44:50 +0100

    T8P1E1

---
+++ Tema8/Prueba/EjemploVentanas/src/ejemploventanas/EjemploVentanas.java
@@ -0,0 +1,22 @@
+
+package ejemploventanas;
+
+import Vista.Ventana1;
+
+/**
+ *
+ * @author 1gdaw04
+ */
+public class EjemploVentanas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Ventana1 v = new Ventana1();
+        v.setVisible(true);
+    }
+    
+}
---
+++ Tema8/T8P1E1/T8P1E1/src/t8p1e1/T8P1E1.java
@@ -0,0 +1,17 @@
+
+package t8p1e1;
+
+import Vista.Ventana1;
+
+public class T8P1E1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Ventana1 v = new Ventana1();
+        v.setVisible(true);
+    }
+    
+}
